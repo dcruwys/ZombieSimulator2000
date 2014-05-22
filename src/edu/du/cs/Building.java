88c4d3693acb7d6a057758140859986297b3f546
Paul Heinen <pheinenjr@gmail.com>
2014-05-21 22:50:23 -0600

    Added Graphics Engine
    Updated some stuff in human and building.

--- src/edu/du/cs/Building.java
+++ src/edu/du/cs/Building.java
@@ -3,10 +3,19 @@ public class Building {
 	
 	private int mySize;
 	private int[][] grid;
+	private int myXCoord;
+	private int myYCoord;
+	private int myType;
+	
+	
 
-	public Building(){
+	public Building(int x, int y, int t){
 		mySize = 100;
 		grid = new int[mySize][mySize];
+		//Configure this shit Dan
+		myXCoord = x;
+		myYCoord = y;
+		myType = t;
 	}
 	
 	public int getMySize() {
@@ -14,6 +23,24 @@ public class Building {
 	}
 	public void setMySize(int mySize) {
 		this.mySize = mySize;
+	}
+	public int getMyXCoord() {
+		return myXCoord;
+	}
+	public void setMyXCoord(int myXCoord) {
+		this.myXCoord = myXCoord;
+	}
+	public int getMyYCoord() {
+		return myYCoord;
+	}
+	public void setMyYCoord(int myYCoord) {
+		this.myYCoord = myYCoord;
+	}
+	public int getMyType() {
+		return myType;
+	}
+	public void setMyType(int myType) {
+		this.myType = myType;
 	}
 	
 	public int[][] getGrid(){
