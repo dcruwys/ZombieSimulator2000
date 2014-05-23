e210be73f6062a91a059b6ba5896b7b782a9264b
Dan Cruwys <dcruwys@gmail.com>
2014-05-22 19:16:53 -0600

    new building generation

--- src/edu/du/cs/Simulate.java
+++ src/edu/du/cs/Simulate.java
@@ -23,17 +23,25 @@ public class Simulate {
 		//second pass
 		for (int row=0; row < mySize; row ++){
 			for (int col=0; col < mySize; col += 5){
+				if(grid[row][col] == 0){
+					grid[row][col] = 9;
+				}
+				else{
 				   grid[row][col] = 0;
+				}
 			}
 		}
 		//generation
 		for (int row=0; row < mySize; row ++){
 			for (int col=0; col < mySize; col ++){
-				if(grid[row][col] != 0 && (int) Math.ceil(Math.random()*100) > 10){
+				if(grid[row][col] != 0 && grid[row][col] != 9 && (int) Math.ceil(Math.random()*100) > 10){
 				   grid[row][col] = 1;
 				}
+				if(grid[row][col] == 3)
+					grid[row][col] = 0;
 			}
 		}
 	}
+	
 
 }
