193c7174b3a9cff923443d4061b0fe45f6f1ab9d
Paul Heinen <pheinenjr@gmail.com>
2014-05-22 18:01:48 -0600

    oijafoiawejfioaweijfkae

---
+++ src/edu/du/cs/Building.java
@@ -0,0 +1,51 @@
+package edu.du.cs;
+
+//Create a simulation class to handle all generation stuff.
+public class Building {
+	
+	private int mySize;
+	private int myXCoord;
+	private int myYCoord;
+	private int myType;
+	
+	
+
+	public Building(){//int x, int y, int t
+		//mySize = 100;
+		
+		//Configure this shit Dan
+//		myXCoord = x;
+//		myYCoord = y;
+//		myType = t;
+	}
+	
+	public int getMySize() {
+		return mySize;
+	}
+	public void setMySize(int mySize) {
+		this.mySize = mySize;
+	}
+	public int getMyXCoord() {
+		return myXCoord;
+	}
+	public void setMyXCoord(int myXCoord) {
+		this.myXCoord = myXCoord;
+	}
+	public int getMyYCoord() {
+		return myYCoord;
+	}
+	public void setMyYCoord(int myYCoord) {
+		this.myYCoord = myYCoord;
+	}
+	public int getMyType() {
+		return myType;
+	}
+	public void setMyType(int myType) {
+		this.myType = myType;
+	}
+	
+//	public int[][] getGrid(){
+//		return grid;
+//	}
+	
+}
