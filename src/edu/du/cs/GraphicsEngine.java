dbf0b03a3c76a22bb08f04664f49e90f2bfe7c8a
Dan Cruwys <dcruwys@gmail.com>
2014-05-23 18:05:10 -0600

    humus

--- src/edu/du/cs/GraphicsEngine.java
+++ src/edu/du/cs/GraphicsEngine.java
@@ -40,18 +40,33 @@ public class GraphicsEngine implements GraphicsInterface
 		for(int row=0;row<grid.length; row++){
 			   for(int col=0;col<grid.length;col++){
 			     switch(grid[row][col]){
-			      case 0:StdDraw.setPenColor( StdDraw.GRAY );
-			      		StdDraw.filledSquare(row+4, col+4, 4);
+			      case 0:StdDraw.setPenColor( StdDraw.LIGHT_GRAY );
+			      		StdDraw.filledSquare(row*10, col*10, 10);
 			       break;
 			      case 1:StdDraw.setPenColor( StdDraw.BLACK );
-		      		StdDraw.filledSquare(row+4, col+4, 4);;
+		      		StdDraw.filledSquare(row*10, col*10, 10);;
 			       break;
 			      case 9:StdDraw.setPenColor( StdDraw.LIGHT_GRAY );
-		      		StdDraw.filledSquare(row+4, col+4, 4);;
+		      		StdDraw.filledSquare(row*10, col*10, 10);;
 			       break;
 			     }
 			   }  
-			}
-	
+		}
 	}
+	public static void main(String[] args){
+		StdDraw.setCanvasSize(500,500);
+		StdDraw.setXscale(0.0, 500.0);
+		StdDraw.setYscale(0.0, 500.0);
+		Simulate test = new Simulate();
+		test.generateBuildings();
+		
+		GraphicsEngine g = new GraphicsEngine();
+		g.drawMap(test.getGrid());
+		Human paul = new Human(10, 10);
+		while(true){
+			paul.move();
+			g.drawHuman(paul);
+		}
+	}
+	
 }
